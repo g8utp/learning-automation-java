32906294684f076973f6c0f3b1da8825043e77ea
g8utp <antonlebedevw@gmail.com>
2026-01-12 02:37:36 +0700

    Fix: remove hardcoded headless config

--- src/test/java/swagTests/Tests/BaseTest.java
+++ src/test/java/swagTests/Tests/BaseTest.java
@@ -6,17 +6,33 @@ import org.junit.jupiter.api.BeforeAll;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-public abstract class BaseTest {
-        @BeforeAll
-        @Step("Базовые настройки для теста")
-    public static void setUp(){
-            Configuration.browser = "chrome";
-            Configuration.browserSize = "1920x1080";
+
+public abstract class BaseTest  {
+    @BeforeAll
+    @Step("Базовые настройки для теста")
+    public static void setUp() {
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
 //            Configuration.headless = false;
-            Configuration.baseUrl = "https://www.saucedemo.com";
-            SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
-                    .screenshots(true)
-                    .savePageSource(true));
+        Configuration.baseUrl = "https://www.saucedemo.com";
+//        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
+//                .screenshots(true)
+//                .savePageSource(true));
+// Проверяем: если мы на сервере CI (GitHub Actions), то включаем Headless
+        String isCiServer = System.getenv("CI"); // Получаем переменную среды
+        if (isCiServer != null) {
+            Configuration.headless = true;
+
+            // Важные настройки для стабильности Chrome в контейнерах (Linux)
+            Configuration.browserCapabilities.setCapability("goog:chromeOptions",
+                    java.util.Map.of("args", java.util.List.of(
+                            "--no-sandbox",
+                            "--disable-dev-shm-usage",
+                            "--disable-gpu"
+                    ))
+            );
         }
+    }
 
 }
+
